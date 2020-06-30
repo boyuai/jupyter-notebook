a0badf827cf4528ee7bc2873abe658f63a3633ac
wangsijie <i@sijie.wang>
2020-06-30 12:18:12 +0800

    deploy command

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+docker build -t 527345677879.dkr.ecr.cn-northwest-1.amazonaws.com.cn/notebook .
+docker push 527345677879.dkr.ecr.cn-northwest-1.amazonaws.com.cn/notebook
---
+++ ecr-login.sh
@@ -0,0 +1,1 @@
+aws ecr get-login-password --region cn-northwest-1 | docker login --username AWS --password-stdin 527345677879.dkr.ecr.cn-northwest-1.amazonaws.com.cn
