7d001173137cc068074e12d90007fb647ff341ca
wangsijie <i@sijie.wang>
2021-02-26 19:27:36 +0800

    ghcr

---
+++ deploy-from-ghcr.sh
@@ -0,0 +1,6 @@
+docker pull ghcr.io/boyuai/notebook
+docker tag ghcr.io/boyuai/notebook 527345677879.dkr.ecr.cn-northwest-1.amazonaws.com.cn/notebook
+docker push 527345677879.dkr.ecr.cn-northwest-1.amazonaws.com.cn/notebook
+docker pull ghcr.io/boyuai/notebook-cpp
+docker tag ghcr.io/boyuai/notebook-cpp 527345677879.dkr.ecr.cn-northwest-1.amazonaws.com.cn/notebook-cpp
+docker push 527345677879.dkr.ecr.cn-northwest-1.amazonaws.com.cn/notebook-cpp
